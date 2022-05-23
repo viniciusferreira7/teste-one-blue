6f1b8d98be05c4641a7d1f86ec35d2340bec9621
Vinicius Ferreira <viniciuferreira32@gmail.com>
2022-05-22 22:46:06 -0300

    criado o globalstyles

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { GlobalStyles } from './styles/GlobalStyles';
 import { Home } from './templetes/Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Home />
+    <GlobalStyles />
   </React.StrictMode>,
 );
---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+*{
+ margin:0;
+ padding:0;
+ box-sizing:border-box ;
+}
+
+html{
+  font-size: 62.5%;
+}
+
+body{
+  background:#1c1a1a;
+}`;
